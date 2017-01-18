806d87b9352f1491ac3d94af83f78ba9a87a1b65
Gordon Hall <gordon@gordonwritescode.com>
2017-01-18 14:55:17 -0500

    logging

--- methods/report/put.js
+++ methods/report/put.js
@@ -17,12 +17,14 @@ module.exports = (req, res) => {
   let report;
   try {
     report = Report.create(req.body.params.message);
+    console.log(report)
   } catch (e) {
     return handleError(req, res, e);
   }
-
+  console.log(report)
   report.requestID = req.body.id;
   report.save((err) => {
+    console.log(err)
     if (err) {
       handleError(req, res, err);
     } else {
